bc850d43d5aa15c95020c329a5d41ccc859d373a
Melanie Massey <nanie_d_enfer@hotmail.com>
2021-12-15 14:13:06 +0100

    finalisation affichage de la page d'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,31 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
-
-    const picture = `assets/photographers/${portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+class Photographer {
+    constructor(data) {
+        this.name = data.name
+        this.id = data.id
+        this.city = data.city
+        this.country = data.country
+        this.tagline = data.tagline
+        this.price = data.price
+        this.portrait = data.portrait
+    }
+    getUserCardDOM() {
+        const article = document.createElement('article');
+        const picture = `assets/photographers/${this.portrait}`;
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+        const h2 = document.createElement('h2');
+        h2.textContent = this.name;
+        const h3 = document.createElement('h3');
+        h3.textContent = this.city + ', ' + this.country;
+        const h4 = document.createElement('h4');
+        h4.textContent = this.tagline;
+        const p = document.createElement('p');
+        p.textContent = this.price + '€/jour';
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(p);
+        return article;
     }
-    return { name, picture, getUserCardDOM }
 }
